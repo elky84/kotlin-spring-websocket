1c904475e78b36ff53e3c1d40d646c5c28205123
elky <elky84@gmail.com>
2023-01-06 22:06:10 +0900

    Spring Boot 버전 3.0.0 -> 3.0.1, Kotlin 버전 1.7.21 -> 1.8.0

--- backend/build.gradle.kts
+++ backend/build.gradle.kts
@@ -1,12 +1,14 @@
 import org.jetbrains.kotlin.gradle.tasks.KotlinCompile
 
 plugins {
-    id("org.springframework.boot") version "3.0.0"
+    id("org.springframework.boot") version "3.0.1"
     id("io.spring.dependency-management") version "1.1.0"
     id("org.graalvm.buildtools.native") version "0.9.18"
-    kotlin("jvm") version "1.7.21"
-    kotlin("plugin.spring") version "1.7.21"
-    kotlin("plugin.jpa") version "1.7.21"
+    kotlin("jvm") version "1.8.0"
+    kotlin("plugin.spring") version "1.8.0"
+    kotlin("plugin.jpa") version "1.8.0"
+    id("com.google.cloud.tools.jib") version "3.3.1"
+    kotlin("kapt") version "1.8.0"
 }
 
 group = "com.elky"
