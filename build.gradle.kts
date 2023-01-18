d9ce74186523f051610e520dd9b672a0c8555b2c
elky <elky84@gmail.com>
2023-01-18 20:45:16 +0900

    backend 프로젝트를 root로 이동. lombok 제거, 프로젝트 명 변경

--- build.gradle.kts
+++ build.gradle.kts
@@ -37,8 +37,6 @@ dependencies {
     implementation("org.jetbrains.kotlin:kotlin-reflect")
     implementation("org.jetbrains.kotlin:kotlin-stdlib-jdk8")
     implementation("org.jetbrains.kotlinx:kotlinx-coroutines-reactor")
-    compileOnly("org.projectlombok:lombok")
-    annotationProcessor("org.projectlombok:lombok")
 
     testImplementation("org.springframework.boot:spring-boot-starter-test")
     testImplementation("io.projectreactor:reactor-test")
